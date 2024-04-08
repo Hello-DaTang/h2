738202bd350b78ce275c25efd238606ef4aae083
Datang <wwx2011623589@gmail.com>
2024-04-08 07:06:20 -0400

    third commit

--- test/sol_test.cpp
+++ test/sol_test.cpp
@@ -1,19 +1,22 @@
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 #include "sol.h"
 
-
-
-TEST(MaxSubArrayTest, HandlesPositiveNumbers) {
+TEST(MaxSubArrayTest, Condition1) {
     std::vector<int> nums = {1, 2, 3, 4, 5};
     EXPECT_EQ(maxSubArray(nums), 15);
 }
 
-TEST(MaxSubArrayTest, HandlesNegativeNumbers) {
-    std::vector<int> nums = {-1, -2, -3, -4, -5};
-    EXPECT_EQ(maxSubArray(nums), -1);
+TEST(MaxSubArrayTest, Condition2) {
+    std::vector<int> nums = {5, -1, -2, -3, -4};
+    EXPECT_EQ(maxSubArray(nums), 5);
 }
 
-TEST(MaxSubArrayTest, HandlesMixedNumbers) {
-    std::vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    EXPECT_EQ(maxSubArray(nums), 6);
+TEST(MaxSubArrayTest, Condition3) {
+    std::vector<int> nums = {-1, -2, -3, -4, 5};
+    EXPECT_EQ(maxSubArray(nums), 5);
+}
+
+TEST(MaxSubArrayTest, Condition4) {
+    std::vector<int> nums = {-1, -2, -3, -4, -5};
+    EXPECT_EQ(maxSubArray(nums), -1);
 }
